d82bfd381eeeec2db37070e5377df407cab26ae4
sl202194@hotmail.com <sl202194@hotmail.com>
2021-11-17 13:42:35 -0500

    tracker added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    decsiption="it's a wineQ package",
+    author="samfalila",
+    packages=find_packages(),
+    license="MIT"
+)
+
