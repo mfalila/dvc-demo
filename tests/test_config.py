3c879328bb2a3f7799fbe6f0f3c7eceac93f9453
sl202194@hotmail.com <sl202194@hotmail.com>
2021-11-18 12:29:48 -0500

    web structure added

---
+++ tests/test_config.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+
+
+
+class NotInRange(Exception):
+    def __init__(self, message="value not in range"):
+        self.message = message
+        super().__init__(self, message)
+
