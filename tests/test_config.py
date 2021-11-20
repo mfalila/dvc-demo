36ab02abed89a81c77ec278fb9e2ab2b1340f336
sl202194@hotmail.com <sl202194@hotmail.com>
2021-11-19 19:14:22 -0500

    github actions workflow added

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,10 +2,7 @@ import pytest
 
 
 
-
-
 class NotInRange(Exception):
     def __init__(self, message="value not in range"):
         self.message = message
         super().__init__(self, message)
-
